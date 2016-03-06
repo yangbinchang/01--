7719b0a9b02d50ebadce84295563cd018d3a1118
yang <yang@yangdemac.local>
2016-03-06 13:01:51 +0800

    创建项目

---
+++ 01.思埠快购/01.思埠快购/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01.思埠快购
+//
+//  Created by yang on 16/3/6.
+//  Copyright © 2016年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01.思埠快购/01.思埠快购/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01.思埠快购
+//
+//  Created by yang on 16/3/6.
+//  Copyright © 2016年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
